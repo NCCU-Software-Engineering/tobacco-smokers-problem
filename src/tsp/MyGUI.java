f99b6b0265a8714ce57c16b184bd02ec0f9597a5
nidhogg5 <nidhogg5@yahoo.com.tw>
2017-12-21 17:28:15 +0800

    Revert "MATCHEES"
    This reverts commit f7f9896a91a956b1ab9107c4930619d5949ded58.

--- src/tsp/MyGUI.java
+++ src/tsp/MyGUI.java
@@ -2,14 +2,12 @@ package tsp;
 
 import javax.swing.JFrame;
 
-public class MyGUI extends JFrame {
+public class MyGUI {
 	
 	public static void start() {
-		JFrame frame = new JFrame("Tobacco Smokers Problem");
+		JFrame frame=new JFrame("HI");
 	    frame.setSize(1200, 800);
 	    frame.setVisible(true);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
-	    JButton button = new JButton("Click me");
 	}
 }
